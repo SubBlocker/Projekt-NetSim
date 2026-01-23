e647bcc970bdb3d274baa1188eb029cb64f79280
Hubert <h.bryki@gmail.com>
2026-01-23 11:09:59 +0100

    hpp

--- include/package.hpp
+++ include/package.hpp
@@ -3,7 +3,15 @@
 
 class Package {
 public:
+    Package();
+    Package(ElementID id);
+    Package(Package&& package);
+    Package& operator=(Package&& other) noexcept;
+    ElementID get_id() const {return id_;};
 
+    ~Package();
+    Package(const Package&) = delete;
+    Package& operator=(const Package&) = delete;
 private:
-
+    ElementID id_;
 };
