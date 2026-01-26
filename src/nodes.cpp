4e7af050fe2758e5fb971e98a029fd55d0285375
Jakub0071 <jkozinski@student.agh.edu.pl>
2026-01-26 01:56:31 +0100

    update w nodes w celu poprawnego działania factory

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,95 +1,79 @@
 #include "nodes.hpp"
 
-void ReceiverPreferences::add_receiver(IPackageReceiver *r) {
-    std::size_t n = preferences_.size();
-
-    if (!n) {
+void ReceiverPreferences::add_receiver(IPackageReceiver* r) {
+    auto num_of_receivers = preferences_.size();
+    if (num_of_receivers == 0) {
         preferences_[r] = 1.0;
-        return;
-    }
-
-    double probability = 1.0 / (n + 1);
-
-    for (auto& receiver : preferences_) {
-        receiver.second = probability;
-    }
-
-    preferences_[r] = probability;
-
-}
-
-void ReceiverPreferences::remove_receiver(IPackageReceiver *r) {
-    auto receiver_to_remove = preferences_.find(r);
-    if (receiver_to_remove == preferences_.end()) return;
-    preferences_.erase(receiver_to_remove);
-
-    std::size_t n = preferences_.size();
-    if (!n) return;
-
-    double probability = 1.0 / n;
-
-    for (auto& receiver : preferences_) {
-        receiver.second = probability;
+    } else {
+        auto denominator = static_cast<double>(num_of_receivers + 1);
+        for (auto& pair : preferences_) {
+            pair.second = 1.0 / denominator;
+        }
+        preferences_[r] = 1.0 / denominator;
     }
 }
 
-IPackageReceiver *ReceiverPreferences::choose_receiver() {
-    auto probability = pg_();
-    if (probability < 0 || probability > 1) {
-        return nullptr;
-    }
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r) {
+    auto it = preferences_.find(r);
+    if (it == preferences_.end()) return;
 
+    preferences_.erase(it);
+
+    auto num_of_receivers = preferences_.size();
+    if (num_of_receivers == 0) return;
+
+    auto new_probability = 1.0 / static_cast<double>(num_of_receivers);
+    for (auto& pair : preferences_) {
+        pair.second = new_probability;
+    }
+}
+
+IPackageReceiver* ReceiverPreferences::choose_receiver() {
+    auto prob = generate_probability_();
     double distribution = 0.0;
-
-    for (auto& receiver : preferences_) {
-        distribution += receiver.second;
-
-        if (distribution < 0 || distribution > 1) {
-            return nullptr;
-        }
-        if (probability <= distribution) {
-            return receiver.first;
+    for (const auto& pair : preferences_) {
+        distribution += pair.second;
+        if (prob <= distribution) {
+            return pair.first;
         }
     }
-
     return nullptr;
 }
 
 void PackageSender::send_package() {
-    if (!buffer_.has_value()) return;
-
-    IPackageReceiver* receiver = receiver_preferences_.choose_receiver();
-    receiver->receive_package(std::move(*buffer_));
-    buffer_.reset();
+    if (buffer_) {
+        IPackageReceiver* receiver = receiver_preferences_.choose_receiver();
+        if (receiver) {
+            receiver->receive_package(std::move(*buffer_));
+            buffer_.reset();
+        }
+    }
 }
 
 void Ramp::deliver_goods(Time t) {
-    if (!buffer_) {
-        push_package(Package());
-        buffer_.emplace(id_);
-        t_ = t;
-        return;
-    }
-
-    if (t == t_ + di_) {
+    if ((t - 1) % di_ == 0) {
         push_package(Package());
     }
 }
 
-Worker::Worker(ElementID id, TimeOffset process_time, std::unique_ptr<IPackageQueue> q): 
-    id_(id), 
-    process_time_(process_time),
-    q_(std::move(q)){
-    };
-
 void Worker::do_work(Time t) {
-    if (!q_->empty() && !currently_processed_.has_value()) {
-        start_time_ = t;
-        currently_processed_.emplace(q_->pop());
+    if (!buffer_ && !q_->empty()) {
+        buffer_.emplace(q_->pop());
+        t_ = t;
     }
-    if (t - start_time_ + 1 >= process_time_) {
-        push_package(std::move(currently_processed_.value()));
-        currently_processed_.reset();
+
+    if (buffer_) {
+        if (t - t_ + 1 == pd_) {
+            push_package(std::move(*buffer_));
+            buffer_.reset();
+        }
     }
-    
+}
+
+void Worker::receive_package(Package&& p) {
+    q_->push(std::move(p));
+}
+
+void Storehouse::receive_package(Package&& p) {
+    d_->push(std::move(p));
 }
