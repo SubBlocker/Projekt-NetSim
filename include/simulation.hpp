52a80b6457538d9b3801f95f37fd83a7b5f6ff38
SubBlocker <szymibas@gmail.com>
2026-02-01 09:55:43 +0100

    simulation init

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -3,8 +3,30 @@
 
 #include "factory.hpp"
 
-void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)>);
 
+void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)>&& rf);
+
+
+class SpecificTurnsReportNotifier {
+public:
+    explicit SpecificTurnsReportNotifier(const std::set<Time>& turns) : turns_(turns) {};
+
+    bool should_generate_report(Time t);
+
+private:
+    std::set<Time> turns_;
+};
+
+
+class IntervalReportNotifier {
+public:
+    explicit IntervalReportNotifier(TimeOffset to) : to_(to) {};
+
+    bool should_generate_report(Time t);
+
+private:
+    TimeOffset to_;
+};
 
 
 #endif //SIMULATION_HXX
--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,5 +1,12 @@
 #include "simulation.hpp"
 
-void simulate(Factory& f, TimeOffset d, std::function<void (Factory&, Time)>){
-    
+void simulate(Factory &f, TimeOffset d, std::function<void(Factory &, Time)> &&rf) {
+}
+
+bool SpecificTurnsReportNotifier::should_generate_report(Time t) {
+    return turns_.find(t) != turns_.cend();
+}
+
+bool IntervalReportNotifier::should_generate_report(Time t) {
+    return t == 1 || t % to_ == 1;
 }
